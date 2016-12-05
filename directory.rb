030529fe411f993a999d708a063bddb9862b4d29
Rob Brentnall <rob.brentnall@gmail.com>
2016-12-05 17:48:37 +0000

    Chapter 8 - Exercise 6. Output now centered

--- directory.rb
+++ directory.rb
@@ -1,43 +1,47 @@
 def input_students
-    puts "Please enter the details of the students"
-    puts "To finish, just hit return twice"
+    # get height and width of terminal window for center
+    h, w = `stty size`.split.map{|e| e.to_i}
+    puts "\n"
+    puts "Please enter the details of the students".center(w)
+    puts "To finish, just hit return twice".center(w)
+    puts "\n"
     # create an empty array
     students = []
     question = "Please enter the student's"
     1.times do
-        puts "#{question} name"
+        puts "#{question} name".center(w)
         name = gets.chomp
         break if name.empty?
-        puts "#{question} age"
+        puts "#{question} age".center(w)
         age = gets.chomp
         break if age.empty?
-        puts "#{question} height"
+        puts "#{question} height".center(w)
         height = gets.chomp
         break if height.empty?
-        puts "#{question} birthplace"
+        puts "#{question} birthplace".center(w)
         birthplace = gets.chomp
         break if birthplace.empty?
-        puts "#{question} hobbies"
+        puts "#{question} hobbies".center(w)
         hobbies = gets.chomp
         break if hobbies.empty?
         while !name.empty? do
             # add the student hash to the array
             students << {name: name, cohort: :november, age: age, height: height, birthplace: birthplace, hobbies: hobbies}
-            puts "Now we have #{students.count} students\n\n"
+            puts "Now we have #{students.count} students\n\n".center(w)
             # get another name from the user
-            puts "#{question} name"
+            puts "#{question} name".center(w)
             name = gets.chomp
             break if name.empty?
-            puts "#{question} age"
+            puts "#{question} age".center(w)
             age = gets.chomp
             break if age.empty?
-            puts "#{question} height"
+            puts "#{question} height".center(w)
             height = gets.chomp
             break if height.empty?
-            puts "#{question} birthplace"
+            puts "#{question} birthplace".center(w)
             birthplace = gets.chomp
             break if birthplace.empty?
-            puts "#{question} hobbies"
+            puts "#{question} hobbies".center(w)
             hobbies = gets.chomp
             break if hobbies.empty?
         end
