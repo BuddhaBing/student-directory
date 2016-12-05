559cf8fe73cc118fefa370a1b16dbba3018a2d2d
Rob Brentnall <rob.brentnall@gmail.com>
2016-12-05 18:04:05 +0000

    Chapter 8 - Exercise 6 refactoring. Output now fully centered

--- directory.rb
+++ directory.rb
@@ -1,47 +1,47 @@
+@h, @w = `stty size`.split.map{|e| e.to_i}
 def input_students
     # get height and width of terminal window for center
-    h, w = `stty size`.split.map{|e| e.to_i}
     puts "\n"
-    puts "Please enter the details of the students".center(w)
-    puts "To finish, just hit return twice".center(w)
+    puts "Please enter the details of the students".center(@w)
+    puts "To finish, just hit return twice".center(@w)
     puts "\n"
     # create an empty array
     students = []
     question = "Please enter the student's"
     1.times do
-        puts "#{question} name".center(w)
+        puts "#{question} name".center(@w)
         name = gets.chomp
         break if name.empty?
-        puts "#{question} age".center(w)
+        puts "#{question} age".center(@w)
         age = gets.chomp
         break if age.empty?
-        puts "#{question} height".center(w)
+        puts "#{question} height".center(@w)
         height = gets.chomp
         break if height.empty?
-        puts "#{question} birthplace".center(w)
+        puts "#{question} birthplace".center(@w)
         birthplace = gets.chomp
         break if birthplace.empty?
-        puts "#{question} hobbies".center(w)
+        puts "#{question} hobbies".center(@w)
         hobbies = gets.chomp
         break if hobbies.empty?
         while !name.empty? do
             # add the student hash to the array
             students << {name: name, cohort: :november, age: age, height: height, birthplace: birthplace, hobbies: hobbies}
-            puts "Now we have #{students.count} students\n\n".center(w)
+            puts "Now we have #{students.count} students\n\n".center(@w)
             # get another name from the user
-            puts "#{question} name".center(w)
+            puts "#{question} name".center(@w)
             name = gets.chomp
             break if name.empty?
-            puts "#{question} age".center(w)
+            puts "#{question} age".center(@w)
             age = gets.chomp
             break if age.empty?
-            puts "#{question} height".center(w)
+            puts "#{question} height".center(@w)
             height = gets.chomp
             break if height.empty?
-            puts "#{question} birthplace".center(w)
+            puts "#{question} birthplace".center(@w)
             birthplace = gets.chomp
             break if birthplace.empty?
-            puts "#{question} hobbies".center(w)
+            puts "#{question} hobbies".center(@w)
             hobbies = gets.chomp
             break if hobbies.empty?
         end
@@ -51,20 +51,22 @@ def input_students
 end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(@w)
+    puts "-------------".center(@w)
 end
 
 def print(students)
     index = 0
     while index < students.size
-        puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort].capitalize} cohort) - Age: #{students[index][:age]}, Height: #{students[index][:height]}, Birthplace: #{students[index][:birthplace]}, Hobbies: #{students[index][:hobbies]}"
+        puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort].capitalize} cohort) - Age: #{students[index][:age]}, Height: #{students[index][:height]}, Birthplace: #{students[index][:birthplace]}, Hobbies: #{students[index][:hobbies]}".center(@w)
         index += 1
     end
 end
 
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    puts "\n"
+    puts "Overall, we have #{names.count} great students".center(@w)
+    puts "\n"
 end
 
 students = input_students
