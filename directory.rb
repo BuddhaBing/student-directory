b446140eccaa4960cef606306cbf87198c42834f
Rob Brentnall <rob.brentnall@gmail.com>
2016-12-08 13:58:20 +0000

    Chapter 11. Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -5,38 +5,33 @@
 
 @cohorts = %w(January February March April May June July August September October November December)
 
-
-def print_menu
-    puts "1. Input the students".center(@w)
-    puts "2. Show the students".center(@w)
-    puts "9. Exit".center(@w) # 9 because we'll be adding more items
-end
-
-def show_students
-    print_header
-    print_students
-    print_students_by_cohort
-    print_footer
-end
-
-def process(selection)
-   case selection
-        when "1"
-            input_students
-        when "2"
-            show_students
-        when "9"
-            exit
-        else
-            puts "I don't know what you meant, try again"
-    end 
-end
-
 def interactive_menu
 	loop do
 	    print_menu
         process(gets.chomp)
     end
+end
+
+def print_menu
+    puts "1. Input the students".center(@w)
+    puts "2. Show the students".center(@w)
+    puts "3. Save the list to students.csv".center(@w)
+    puts "9. Exit".center(@w) # 9 because we'll be adding more items
+end
+
+def process(selection)
+    case selection
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "3"
+            save_students
+        when "9"
+            exit
+        else
+            puts "I don't know what you meant, try again"
+    end 
 end
 
 def input_students
@@ -99,6 +94,13 @@ def input_students
 
 end
 
+def show_students
+    print_header
+    print_students
+    print_students_by_cohort
+    print_footer
+end
+
 def print_header
     puts "\n\n"
     header = "The Students of Villains Academy"
@@ -146,6 +148,17 @@ def print_footer
         puts "\n"
     end
 end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+
 
 interactive_menu 
 
